0bdb82b130fcb6c6605044727c8193d77e61d9e4
kaplone <dvd.brtrnd@gmail.com>
2016-01-15 11:56:08 +0100

    nouvelle icone

--- LequelFX_etiquettes/src/main/java/LequelFX_etiquettes/Main.java
+++ LequelFX_etiquettes/src/main/java/LequelFX_etiquettes/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
@@ -14,6 +15,10 @@ public class Main extends Application {
 			Scene scene = new Scene((Parent) JfxUtils.loadFxml("GUI_etiquettes.fxml"), 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			primaryStage.getIcons().add(new Image(getClass().getResource("LequelFX_etiquette_01.png").toExternalForm()));
+			primaryStage.setTitle("LequelFX_etiquettes");
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
